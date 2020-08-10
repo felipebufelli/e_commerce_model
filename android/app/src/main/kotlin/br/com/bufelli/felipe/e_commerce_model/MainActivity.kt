f08680d9d840bd094f6ed599408b95763b1fcdbe
Felipe <felipe.bufelli@gmail.com>
2020-08-10 15:51:42 -0300

    Creating project

---
+++ android/app/src/main/kotlin/br/com/bufelli/felipe/e_commerce_model/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.bufelli.felipe.e_commerce_model
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
