446ffc1669aac14bd5462285387f09c5c9b4ed4d
Felipe <felipe.bufelli@gmail.com>
2020-08-25 09:55:18 -0300

    Documents for Products

--- lib/models/products_manager.dart
+++ lib/models/products_manager.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_commerce_model/models/product.dart';
+import 'package:flutter/foundation.dart';
 
-class ProductManager {
+class ProductManager extends ChangeNotifier {
 
   ProductManager() {
     _loadAllProducts();
@@ -8,8 +10,17 @@ class ProductManager {
 
   final Firestore firestore = Firestore.instance;
 
+  List<Product> _allProducts = [];
+
   Future<void> _loadAllProducts() async {
     final QuerySnapshot snapProducts = await firestore.collection('products').getDocuments();
+
+    _allProducts = snapProducts.documents.map(
+      (d) => Product.fromDocument(d)
+    ).toList();
+
+    notifyListeners();
   }
+
 
 }
