b250e8865b6a0a2c8760b266778e904e9985ba90
Felipe <felipe.bufelli@gmail.com>
2020-08-27 14:48:37 -0300

    Updating items in the cart

--- lib/models/cart_manager.dart
+++ lib/models/cart_manager.dart
@@ -1,10 +1,13 @@
+import 'dart:html';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:e_commerce_model/models/cart_product.dart';
 import 'package:e_commerce_model/models/product.dart';
 import 'package:e_commerce_model/models/user.dart';
 import 'package:e_commerce_model/models/user_manager.dart';
+import 'package:flutter/foundation.dart';
 
-class CartManager  {
+class CartManager extends ChangeNotifier {
 
   List<CartProduct> items = [];
 
@@ -29,21 +32,41 @@ class CartManager  {
   }
 
   void addToCart(Product product) {
-
     try {
       final e = items.firstWhere((p) => p.stackable(product));
-      e.quantity++;
+      e.increment();
     } catch(e) {
       final cartProduct = CartProduct.fromProduct(product);
       cartProduct.addListener(_onItemUpdated);
       items.add(cartProduct);
-      user.cartReference.add(cartProduct.toCartItemMap());
+      user.cartReference.add(cartProduct.toCartItemMap())
+        .then((doc) => cartProduct.id = doc.documentID);
     }
+    notifyListeners();
+  }
 
+  void removeFromCart(CartProduct cartProduct) {
+    items.removeWhere(
+      (p) => p.id == cartProduct.id
+    );
+    user.cartReference.document(cartProduct.id).delete();
+    cartProduct.removeListener(_onItemUpdated);
+    notifyListeners();
   }
 
   void _onItemUpdated(){
-    
+    for(final cartProduct in items) {
+      if(cartProduct.quantity == 0) {
+        removeFromCart(cartProduct);
+      } else {
+        _updateCartProduct(cartProduct);
+      }
+    }
+  }
+
+  void _updateCartProduct(CartProduct cartProduct){
+    user.cartReference.document(cartProduct.id)
+      .updateData(cartProduct.toCartItemMap());
   }
 
 }
